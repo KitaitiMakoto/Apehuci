d060df3ac765ad339e7f07efd2a83afe549db4bd
KITAITI Makoto <KitaitiMakoto@gmail.com>
2016-02-03 13:57:37 +0900

    Install groonag-httpd

--- recipe.rb
+++ recipe.rb
@@ -10,3 +10,24 @@ end
 execute 'apt-get update' do
   action :nothing
 end
+
+execute 'add-apt-repository -y universe' do
+  not_if 'grep -E "^deb .+ wily universe$" /etc/apt/sources.list'
+end
+execute 'add-apt-repository -y ppa:groonga/ppa' do
+  not_if 'test -e /etc/apt/sources.list.d/groonga-ubuntu-ppa-wily.list'
+  notifies :run, 'execute[apt-get update]', :immediately
+end
+
+%w[
+  groonga
+  groonga-tokenizer-mecab
+  groonga-token-filter-stem
+  groonga-httpd
+].each do |pkg|
+  package pkg
+end
+
+service 'groonga-httpd' do
+  action [:enable, :start]
+end
