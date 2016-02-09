ae41cefcd0518a2853266039bc658d5cc62bd9ee
KITAITI Makoto <KitaitiMakoto@gmail.com>
2016-02-09 14:04:36 +0900

    Install Let's Encrypt

--- server/recipe.rb
+++ server/recipe.rb
@@ -52,3 +52,13 @@ execute 'reload groonga-httpd' do
   action :nothing
   command '/usr/sbin/groonga-httpd -t && /usr/sbin/groonga-httpd -s reload'
 end
+
+git '/root/letsencrypt' do
+  repository 'https://github.com/letsencrypt/letsencrypt'
+end
+
+execute 'install letsencrypt' do
+  command './letsencrypt-auto certonly --webroot --non-interactive --domains search.apehuci.kitaitimakoto.net --email KitaitiMakoto@gmail.com --agree-tos --webroot-path /usr/share/groonga/html'
+  cwd '/root/letsencrypt'
+  not_if 'test -e /etc/letsencrypt/live/search.apehuci.kitaitimakoto.net/cert.pem'
+end
